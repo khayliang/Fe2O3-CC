a9bc6d08cd25668a1f8fbd794a09e9df652e7f1c
khayliang <khayliang@gmail.com>
2021-05-19 17:59:06 +0800

    refactor syntax nodes

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,50 +1,68 @@
 pub trait Node {}
 
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum Type {
+    Integer(i32),
+}
+
+pub trait Expression {
+    fn evaluate(&self) -> Type;
+}
+impl Node for dyn Expression {}
 pub mod expressions {
+    use super::*;
     pub struct Constant {
-        value: u32,
+        value: Type,
     }
-    impl super::Node for Constant {}
     impl Constant {
-        pub fn new(value: u32) -> Constant {
+        pub fn new(value: Type) -> Constant {
             Constant { value }
         }
     }
-}
-
-pub mod statements {
-    pub struct Return {
-        value: super::expressions::Constant,
-    }
-    impl super::Node for Return {}
-    impl Return {
-        pub fn new(value: super::expressions::Constant) -> Return {
-            Return { value }
+    impl Expression for Constant {
+        fn evaluate(&self) -> Type {
+            return self.value;
         }
     }
 }
+pub trait Statement {}
+impl Node for dyn Statement {}
+pub mod statements {
+    use super::*;
+    pub struct Return {
+        expression: Box<Expression>,
+    }
+    impl Return {
+        pub fn new(expression: Box<Expression>) -> Return {
+            Return { expression }
+        }
+    }
+    impl Statement for Return {}
+}
 
 pub struct Function {
+    return_type: Type,
     name: String,
-    body: statements::Return,
+    body: Vec<Box<Statement>>,
 }
 impl Node for Function {}
 impl Function {
-    pub fn new(name: &str, body: statements::Return) -> Function {
+    pub fn new(return_type: Type, name: String, body: Vec<Box<Statement>>) -> Function {
         Function {
-            name: name.to_string(),
+            return_type,
+            name: name,
             body,
         }
     }
 }
 
 pub struct Program {
-    value: Function,
+    root: Function,
 }
 impl Node for Program {}
 impl Program {
-    pub fn new(value: Function) -> Program {
-        Program { value }
+    pub fn new(root: Function) -> Program {
+        Program { root }
     }
 }
 
@@ -53,10 +71,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_program_node() {
-        let expression = expressions::Constant::new(2);
-        let statement = statements::Return::new(expression);
-        let main_function = Function::new("main", statement);
-        let main_program = Program::new(main_function);
+    fn test_integer_variable() {
+        let integer = Type::Integer(2);
+        let integer_enum = Type::Integer;
+    }
+
+    #[test]
+    fn test_constant_expression() {
+        let integer = Type::Integer(2);
+        let constant = expressions::Constant::new(integer);
+        assert_eq!(integer, constant.evaluate());
+    }
+
+    #[test]
+    fn test_program() {
+        let integer = Type::Integer(2);
+        let constant = expressions::Constant::new(integer);
+        let return_statement = statements::Return::new(Box::new(constant));
+
+        let identifier = String::from("main");
+        let body: Vec<Box<Statement>> = vec![Box::new(return_statement)];
+        let return_type = Type::Integer(0);
+        let function = Function::new(return_type, identifier, body);
+        let main_program = Program::new(function);
     }
 }
