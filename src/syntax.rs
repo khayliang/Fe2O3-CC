b272d906a78802880a95bfb0f3d31e3c9cdb9dab
khayliang <khayliang@gmail.com>
2021-05-27 00:45:06 +0800

    all nodes have to_asm fn

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,9 +1,11 @@
 use std::fmt;
 
+use indoc::formatdoc;
 use textwrap::indent;
 
 pub trait Node: fmt::Display {
     fn type_of(&self) -> &'static str;
+    fn to_asm(&self) -> String;
 }
 
 // TODO: Create a variable struct that has type Type Enum
@@ -22,6 +24,11 @@ impl Node for Type {
     fn type_of(&self) -> &'static str {
         match self {
             Self::Integer(_) => "Integer",
+        }
+    }
+    fn to_asm(&self) -> String {
+        match self {
+            Self::Integer(val) => return format!("${}", val),
         }
     }
 }
@@ -53,6 +60,9 @@ pub mod expressions {
         fn type_of(&self) -> &'static str {
             "Constant"
         }
+        fn to_asm(&self) -> String {
+            return self.value.to_asm();
+        }
     }
     impl Expression for Constant {
         fn evaluate(&self) -> Type {
@@ -75,8 +85,14 @@ pub enum Statement {
 impl Node for Statement {
     fn type_of(&self) -> &'static str {
         match self {
-            Self::Function(_) => "Function",
-            Self::Return(_) => "Return",
+            Self::Function(val) => val.type_of(),
+            Self::Return(val) => val.type_of(),
+        }
+    }
+    fn to_asm(&self) -> String {
+        match self {
+            Self::Function(val) => val.to_asm(),
+            Self::Return(val) => val.to_asm(),
         }
     }
 }
@@ -88,6 +104,7 @@ impl fmt::Display for Statement {
         }
     }
 }
+trait StatementTrait: Node {}
 
 pub mod statements {
     use super::*;
@@ -97,6 +114,22 @@ pub mod statements {
     impl Return {
         pub fn new(expression: Box<dyn Expression>) -> Statement {
             Statement::Return(Return { expression })
+        }
+    }
+    impl StatementTrait for Return {}
+    impl Node for Return {
+        fn type_of(&self) -> &'static str {
+            "Return"
+        }
+        fn to_asm(&self) -> String {
+            let return_expression_asm = self.expression.to_asm();
+            let return_asm = formatdoc! {"
+                movl {}, %eax
+                ret
+                ",
+                return_expression_asm
+            };
+            return return_asm;
         }
     }
 
@@ -119,6 +152,26 @@ pub mod statements {
                 name: name,
                 body,
             })
+        }
+    }
+    impl StatementTrait for Function {}
+    impl Node for Function {
+        fn type_of(&self) -> &'static str {
+            "Function"
+        }
+        fn to_asm(&self) -> String {
+            let asm_body: Vec<String> = self
+                .body
+                .iter()
+                .map(|statement| statement.to_asm())
+                .rev()
+                .collect();
+            let mut function_asm = formatdoc! {"
+                .globl _{name}
+                _{name}:
+            ", name=self.name};
+            asm_body.iter().for_each(|asm| function_asm.push_str(&asm));
+            return function_asm;
         }
     }
     impl fmt::Display for Function {
@@ -154,6 +207,9 @@ impl Program {
 impl Node for Program {
     fn type_of(&self) -> &'static str {
         "Program"
+    }
+    fn to_asm(&self) -> String {
+        self.root.to_asm()
     }
 }
 impl fmt::Display for Program {
@@ -201,6 +257,7 @@ mod tests {
         let integer_formatted: String = format!("{}", integer);
         assert_eq!("Integer", integer.type_of());
         assert_eq!("Integer<2>", &integer_formatted);
+        assert_eq!("$2", integer.to_asm());
     }
 
     #[test]
@@ -211,6 +268,7 @@ mod tests {
         assert_eq!(integer, constant.evaluate());
         assert_eq!("Constant", constant.type_of());
         assert_eq!("Constant Integer<2>", constant_formatted);
+        assert_eq!("$2", constant.to_asm());
     }
 
     #[test]
@@ -219,6 +277,11 @@ mod tests {
         let return_formatted: String = format!("{}", return_statement);
         assert_eq!("Return", return_statement.type_of());
         assert_eq!("Return Constant Integer<2>", return_formatted);
+        let expected_asm = indoc! {"
+            movl $2, %eax
+            ret
+        "};
+        assert_eq!(expected_asm, return_statement.to_asm());
     }
 
     #[test]
@@ -230,7 +293,14 @@ mod tests {
                 body:
                     Return Constant Integer<2>
         "};
+        let expected_asm = indoc! {"
+            .globl _main
+            _main:
+            movl $2, %eax
+            ret
+        "};
         assert_eq!(expected_format, function_formatted);
+        assert_eq!(expected_asm, function.to_asm());
     }
 
     #[test]
@@ -244,6 +314,13 @@ mod tests {
         "};
         let program_formatted: String = format!("{}", main_program);
         assert_eq!(expected_format, program_formatted);
+        let expected_asm = indoc! {"
+            .globl _main
+            _main:
+            movl $2, %eax
+            ret
+        "};
+        assert_eq!(expected_asm, main_program.to_asm());
     }
 
     #[test]
